520fb1fadd8761a29d6cc57f0fea196d5b167f5c
sarthakeddy <sarthakshukla.eddy@gmail.com>
2020-02-05 04:13:30 +0530

    Add more details

--- gen.py
+++ gen.py
@@ -20,14 +20,40 @@ while True:
 
 link="https://codeforces.com/api/user.info?handles="+cf_id
 ml=requests.get(link)
+c=page_soup.find_all('li')
 p=json.loads(ml.text)
 dic=p['result']
-
-for p in dic:
-	print(dic[p])
+ls=dic[0]
 
 #user-id
-print("User-ID : ",cf_id)
+print("User-ID:",cf_id)
 
 #User Name
-print("Name : ",dic[9]," ",dic[0])
+print("Name:",ls['firstName'],ls['lastName'])
+
+#country
+print("Country:",ls['country'])
+
+#organisation
+print("Institute:",ls['organization'])
+
+#city
+print("City:",ls['city'])
+
+#current rating
+print("Current Rating:",ls['rating'])
+
+#current group
+print("Current Group:",ls['rank'])
+
+#contrib
+print("Contribution:",ls['contribution'])
+
+#max rating
+print("Max Rating:",ls['maxRating'])
+
+#max rank
+print("Max Group:",ls['maxRank'])
+
+#registered
+print("Registered:",c[44].span.text.strip())
